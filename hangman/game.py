1e543b216831f91938e9d6105718517d449fb9d3
mikepeiman <mikepeiman@gmail.com>
2018-04-14 20:33:17 +0000

    Passing half the tests in final test_hangman_game.py, stumped for now - going to copy solution and look at diff

--- hangman/game.py
+++ hangman/game.py
@@ -53,23 +53,77 @@ class GuessWord(object):
         self.answer = word.lower()
         self.mask_char = "*"
         self.masked = len(self.answer) * self.mask_char
-        self.previous_guesses = []
-        self.miss_count = 0
+        # self.previous_guesses = []
+        # self.miss_count = 0
 
     def perform_attempt(self,character):
-        self.previous_guesses.append(character.lower())
         if len(character) != 1:
             raise InvalidGuessedLetterException()
         if character.lower() not in self.answer:
             return GuessAttempt(character, miss=True)
             
-        self.reveal = "".join([character if character in self.previous_guesses else self.mask_char for character in self.answer.lower()])
-        self.masked = self.reveal
+        # reveal = "".join([character if character in self.previous_guesses else self.mask_char for character in self.answer])
+        # self.masked = reveal
+
+        new_word = ''
+
+        for answer_char, masked_char in zip(self.answer, self.masked):
+            if character.lower() == answer_char:
+                new_word += answer_char
+            else:
+                new_word += masked_char
         return GuessAttempt(character, hit=True)
 
 class HangmanGame(object):
-    def __init__(self):
-        pass
+    WORD_LIST = ['rmotr', 'python', 'awesome']
+    
+    def __init__(self, word_list=None,number_of_guesses=5):
+        if not word_list:
+            word_list = self.WORD_LIST
+        self.number_of_guesses = number_of_guesses
+        self.previous_guesses = []
+        self.miss_count = 0
+        self.remaining_misses = self.number_of_guesses - self.miss_count
+        self.word = GuessWord(self.select_random_word(word_list))
+    
+    def is_won(self):
+        return self.word.masked == self.word.answer
+        
+    def is_lost(self):
+        return self.remaining_misses == 0
+    
+    def is_finished(self):
+        return self.is_won() or self.is_lost()
+    
+    def guess(self,character):
+        character = character.lower()
+        if character in self.previous_guesses:
+            raise InvalidGuessedLetterException()
+        
+        if self.is_finished():
+            raise GameFinishedException
+        
+        self.previous_guesses.append(character)
+        attempt = self.word.perform_attempt(character)
+        
+        if attempt.is_miss():
+            self.remaining_misses -= 1
+            
+        if self.is_won():
+            raise GameWonException
+            
+        if self.is_lost():
+            raise GameLostException
+        
+        return attempt
+        
+    @classmethod
+    def select_random_word(cls, word_list):
+        if not word_list:
+            raise InvalidListOfWordsException()
+        return random.choice(word_list)
+
+        
   
 '''
 >we need to create an answer word to guess, from either user input or default list
